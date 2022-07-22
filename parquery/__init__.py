bf0b447fb667a557ae21266267e03214fb52aa8f
Felix Finkbeiner <ffinkbeiner@visualfabriq.com>
2022-07-22 15:05:15 +0200

    Add version from init and attribute. Add pre-release env var, set pre-release env var for CI

--- parquery/__init__.py
+++ parquery/__init__.py
@@ -1,3 +1,7 @@
 from parquery.aggregate import aggregate_pq, aggregate_pa
 from parquery.transport import serialize_df, deserialize_df, serialize_pa_table, deserialize_pa_table
 from parquery.write import df_to_parquet
+import os
+
+pre_release_version = os.getenv('PRE_RELEASE_VERISION', '')
+__version__ = f'0.3.2{pre_release_version}'
