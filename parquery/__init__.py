b29afa69a29a3c325ef19092fbb89530d037ebfc
Felix F <76952670+ffinkbe@users.noreply.github.com>
2022-07-26 16:50:18 +0200

    Apply suggestions from code review
    Co-authored-by: Jelle Verstraaten <72198809+jverstraaten@users.noreply.github.com>

--- parquery/__init__.py
+++ parquery/__init__.py
@@ -3,5 +3,5 @@ from parquery.transport import serialize_df, deserialize_df, serialize_pa_table,
 from parquery.write import df_to_parquet
 import os
 
-pre_release_version = os.getenv('PRE_RELEASE_VERISION', '')
+pre_release_version = os.getenv('PRE_RELEASE_VERSION', '')
 __version__ = '0.3.2{}'.format(pre_release_version)
