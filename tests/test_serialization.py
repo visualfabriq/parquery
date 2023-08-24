78e51db170c84fcdcfae4bd1d70f9ae17188c7ad
Jelle Verstraaten <jverstraaten@visualfabriq.com>
2023-08-24 15:05:56 +0200

    Removed unnecessary test code that causes codacy to complain

--- tests/test_serialization.py
+++ tests/test_serialization.py
@@ -1,14 +1,7 @@
-import os
-import shutil
-import tempfile
-from contextlib import contextmanager
-
 import numpy as np
 import pandas as pd
 import pyarrow as pa
-import six
 
-from parquery.write import df_to_parquet
 from parquery.transport import (
     serialize_df,
     deserialize_df,
@@ -16,37 +9,16 @@ from parquery.transport import (
     deserialize_pa_table
 )
 
+def test_pa_serialization():
+    iterable = ((x, x) for x in range(20000))
+    data = np.fromiter(iterable, dtype='i8,i8')
+    df = pd.DataFrame(data)
 
-class TestSerialization(object):
-    @contextmanager
-    def on_disk_data_cleaner(self, data):
-        # write
-        self.filename = tempfile.mkstemp(prefix='test-')[-1]
-        df_to_parquet(pd.DataFrame(data), self.filename)
+    data_table = pa.Table.from_pandas(df, preserve_index=False)
+    buf = serialize_pa_table(data_table)
+    data_table_2 = deserialize_pa_table(buf)
 
-        yield self.filename
-
-        shutil.rmtree(self.rootdir)
-        self.rootdir = None
-
-    def setup(self):
-        self.filename = None
-
-    def teardown(self):
-        if self.filename:
-            os.remove(self.filename)
-            self.filename = None
-
-    def test_pa_serialization(self):
-        iterable = ((x, x) for x in range(20000))
-        data = np.fromiter(iterable, dtype='i8,i8')
-        df = pd.DataFrame(data)
-
-        data_table = pa.Table.from_pandas(df, preserve_index=False)
-        buf = serialize_pa_table(data_table)
-        data_table_2 = deserialize_pa_table(buf)
-
-        assert data_table == data_table_2
+    assert data_table == data_table_2
 
 
 def test_serialization():
