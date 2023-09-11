e17fa46dc8a7b2e125d6fb494730ccafbd0a8ec6
Jelle Verstraaten <jverstraaten@visualfabriq.com>
2023-09-11 16:27:13 +0200

    Version bump to indicate new backward incompatibility

--- parquery/__init__.py
+++ parquery/__init__.py
@@ -4,7 +4,7 @@ from parquery.write import df_to_parquet
 import os
 
 pre_release_version = os.getenv('PRE_RELEASE_VERSION', '')
-__version__ = pre_release_version if pre_release_version else '0.4.2'
+__version__ = pre_release_version if pre_release_version else '1.0.0'
 
 __all__ = [
     'aggregate_pq',
