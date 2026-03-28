e5da211c8cacc2726295bc1ad0f866d726e807e2
Felix Finkbeiner <ffinkbeiner@visualfabriq.com>
2026-03-28 11:42:52 +0100

    Add memory limit to Duckdb

--- parquery/__init__.py
+++ parquery/__init__.py
@@ -27,7 +27,7 @@ from parquery.transport import (
 from parquery.write import df_to_parquet
 
 pre_release_version = os.getenv("PRE_RELEASE_VERSION", "")
-__version__: str = pre_release_version if pre_release_version else "2.0.6"
+__version__: str = pre_release_version if pre_release_version else "2.0.7"
 
 __all__ = [
     "aggregate_pq",
